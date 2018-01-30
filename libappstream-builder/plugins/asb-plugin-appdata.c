0ce66f1ab784d1bf6da29af290f61b2352aedc8b
Richard Hughes <richard@hughsie.com>
2018-01-30 10:27:25 +0000

    trivial: Allow specifying <icon> tags in metainfo files

--- libappstream-builder/plugins/asb-plugin-appdata.c
+++ libappstream-builder/plugins/asb-plugin-appdata.c
@@ -104,11 +104,6 @@ asb_plugin_process_filename (AsbPlugin *plugin,
 				 as_problem_get_message (problem));
 	}
 
-	/* nuke things that do not belong */
-	icons = as_app_get_icons (AS_APP (app));
-	if (icons->len > 0)
-		g_ptr_array_set_size (icons, 0);
-
 	/* fix up the project license */
 	tmp = as_app_get_project_license (AS_APP (app));
 	if (tmp != NULL && !as_utils_is_spdx_license (tmp)) {
